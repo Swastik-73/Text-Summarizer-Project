7d9cbd3c420b4066d51b1cf70a9a90ff1f79eda7
SWASTIK SENAPATI <swastik.senapati.contractor@pepsico.com>
2024-04-05 18:55:24 +0530

    Data Ingestion Notebook Added

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.textSummarizer.logging import logger
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>>>> {STAGE_NAME} started <<<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>> STAGE {STAGE_NAME} completed <<<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
