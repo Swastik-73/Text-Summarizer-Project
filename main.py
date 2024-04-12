c2393239bc466e2f671315747340df86e1bb956e
SWASTIK SENAPATI <swastik.senapati.contractor@pepsico.com>
2024-04-12 16:13:31 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from src.textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -7,7 +8,7 @@ try:
     logger.info(f">>>>>>>>> {STAGE_NAME} started <<<<<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
-    logger.info(f">>>>>>>>> STAGE {STAGE_NAME} completed <<<<<<<<<<")
+    logger.info(f">>>>>>>>> {STAGE_NAME} completed <<<<<<<<<<")
 except Exception as e:
     logger.exception(e)
     raise e
@@ -17,7 +18,17 @@ try:
     logger.info(f">>>>>>>>> {STAGE_NAME} started <<<<<<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
-    logger.info(f">>>>>>>>> STAGE {STAGE_NAME} completed <<<<<<<<<<")
+    logger.info(f">>>>>>>>> {STAGE_NAME} completed <<<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>>>>> {STAGE_NAME} started <<<<<<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>>>>> {STAGE_NAME} completed <<<<<<<<<<")
 except Exception as e:
     logger.exception(e)
     raise e
